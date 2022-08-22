bde9d83b740b946f185aa9618ed337b63d3a0637
akras <akrasilnikov@meta-devs.io>
2022-08-22 04:36:52 +0300

    Небольшая правка SortingWithComparableTest

--- src/test/java/io/metadevs/akrasilnikov/module_2_5/SortingWithComparableTest.java
+++ src/test/java/io/metadevs/akrasilnikov/module_2_5/SortingWithComparableTest.java
@@ -1,15 +1,20 @@
 package io.metadevs.akrasilnikov.module_2_5;
 
+import io.metadevs.akrasilnikov.OOP.AbstractSpecialist;
 import org.junit.jupiter.api.Test;
 import java.util.Collections;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortingWithComparableTest extends AbstractHiringSpecialists {
 
     @Test
     public void sortingWithComparableTest() {
         hiringSpecialists();
-        companyBender.printSpecialists();
         Collections.sort(companyBender.getSpecialists());
-        companyBender.printSpecialists();
+        for (int i = 0; i < companyBender.getSpecialists().size() - 1; i++) {
+            AbstractSpecialist specialist1 = companyBender.getSpecialists().get(i);
+            AbstractSpecialist specialist2 = companyBender.getSpecialists().get(i + 1);
+            assertThat(specialist1.compareTo(specialist2)).isEqualTo(-1);
+        }
     }
 }
